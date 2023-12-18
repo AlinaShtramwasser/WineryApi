d560e1415f75739138e0b0e41ee9283b318eeac0
AlinaShtramwasser <nakhamchikalina@gmail.com>
2023-12-18 18:50:29 -0500

    code cleanup

--- WineryApi/Controllers/AuthController.cs
+++ WineryApi/Controllers/AuthController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Google.Apis.Auth;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using Google.Apis.Auth;
-using Microsoft.AspNetCore.Mvc;
 using WineryApi.Models;
 using WineryApi.Services;
 
